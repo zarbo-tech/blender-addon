4cb56b0587fdfa2b4796e7adf91e8dc45ab1cca8
Andrew B <Andrux0o@gmail.com>
2023-06-29 14:09:04 +0300

    Панель скрыта, если не введен токен

--- operators/general_panel.py
+++ operators/general_panel.py
@@ -22,51 +22,51 @@ class ZarboPanel(bpy.types.Panel):
             row = layout.row(align=True)
             row.operator("object.reset_api_key")
 
-        layout.prop(context.scene, 'select_file')
+            layout.prop(context.scene, 'select_file')
 
-        if scene.select_file:
-            box = layout.box()
-            box.label(text="Будет отправлен загруженный файл", icon="INFO")
-            box = layout.box()
-            col = box.column()
-            row = col.row()
-            row.label(text="Файл для загрузки:")
-            row.label(text=bpy.context.scene.get('zarbo_file_name'))
-            row = box.row(align=True)
-            row.operator("object.file_loader")
-        else:
-            box = layout.box()
-            box.label(text="Будут отправлены выделенные объекты сцены", icon="INFO")
+            if scene.select_file:
+                box = layout.box()
+                box.label(text="Будет отправлен загруженный файл", icon="INFO")
+                box = layout.box()
+                col = box.column()
+                row = col.row()
+                row.label(text="Файл для загрузки:")
+                row.label(text=bpy.context.scene.get('zarbo_file_name'))
+                row = box.row(align=True)
+                row.operator("object.file_loader")
+            else:
+                box = layout.box()
+                box.label(text="Будут отправлены выделенные объекты сцены", icon="INFO")
 
-        layout.prop(context.scene, 'use_manage_menu')
-        if context.scene.use_manage_menu == True:
+            layout.prop(context.scene, 'use_manage_menu')
+            if context.scene.use_manage_menu == True:
 
-            box = layout.box()
-            box.label(text="Выберите коллекцию и продукт:")
+                box = layout.box()
+                box.label(text="Выберите коллекцию и продукт:")
 
-            # row = layout.row(align=True)
-            box.prop(context.scene, 'collections_enum')
+                # row = layout.row(align=True)
+                box.prop(context.scene, 'collections_enum')
 
-            row = box.row(align=True)
-            row.operator('object.update_collections')
+                row = box.row(align=True)
+                row.operator('object.update_collections')
 
-            box.prop(context.scene, 'products_enum')
+                box.prop(context.scene, 'products_enum')
 
-            row = box.row(align=True)
-            row.operator('object.update_products')
+                row = box.row(align=True)
+                row.operator('object.update_products')
 
 
-        else:
-            box = layout.box()
-            pie = box.menu_pie()
-            pie.label(text='Система сама определит новую или использует', icon="INFO")
-            pie.label(text="существующую коллекцию, в которой будет ")
-            pie.label(text="создан продукт")
+            else:
+                box = layout.box()
+                pie = box.menu_pie()
+                pie.label(text='Система сама определит новую или использует', icon="INFO")
+                pie.label(text="существующую коллекцию, в которой будет ")
+                pie.label(text="создан продукт")
 
-        layout.separator()
-        row = layout.row(align=True)
-        row.operator("object.send_file")
-        row.scale_y = 1.7
+            layout.separator()
+            row = layout.row(align=True)
+            row.operator("object.send_file")
+            row.scale_y = 1.7
 
-        if hasattr(scene, 'widget_url'):
-            layout.prop(context.scene, 'widget_url')
+            if hasattr(scene, 'widget_url'):
+                layout.prop(context.scene, 'widget_url')
